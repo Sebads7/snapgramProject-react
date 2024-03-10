6d7fde82267288e11c5cd73d0c68f0555e76b6dd
Sebas <disalvatore.sebastian@gmail.com>
2024-03-10 00:03:02 -0300

    working on pages and home

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,18 @@ import SignupForm from "./_auth/forms/SignupForm";
 
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
-import { Home } from "./_root/pages";
+import {
+  AllUsers,
+  CreatePost,
+  Explore,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from "./_root/pages";
 import { Toaster } from "@/components/ui/toaster";
+import EditPost from "./_root/pages/EditPost";
 
 // rafce this will create a functional component with the import statement
 
@@ -25,6 +35,14 @@ const App = () => {
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/post/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
 
